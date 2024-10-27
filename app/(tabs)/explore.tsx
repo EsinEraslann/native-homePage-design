ce4c8c4efbabe3ca5553376eea1380110c701bac
EsinEraslann <esineraslanx@gmail.com>
2024-10-27 14:16:44 +0300

    page style fixed

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -12,7 +12,7 @@ export default function TabTwoScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
+      {/* <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Explore</ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
@@ -83,7 +83,7 @@ export default function TabTwoScreen() {
             </ThemedText>
           ),
         })}
-      </Collapsible>
+      </Collapsible> */}
     </ParallaxScrollView>
   );
 }
